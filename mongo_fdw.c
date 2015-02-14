f1f40a42d0e00e2dd3a913c53c2f5cd0d1b4da98
Paul Coppinger <pc@unipagos.com.mx>
2015-02-13 19:46:23 -0600

    revert

--- mongo_fdw.c
+++ mongo_fdw.c
@@ -739,15 +739,7 @@ MongoGetOptions(Oid foreignTableId)
 	}
 
 	username = MongoGetOptionValue(foreignTableId, OPTION_NAME_USERNAME);
-	if (username == NULL)
-	{
-		username = pstrdup("");
-	}
 	password = MongoGetOptionValue(foreignTableId, OPTION_NAME_PASSWORD);
-	if (password == NULL)
-	{
-		password = pstrdup("");
-	}
 
 	mongoFdwOptions = (MongoFdwOptions *) palloc0(sizeof(MongoFdwOptions));
 	mongoFdwOptions->addressName = addressName;
